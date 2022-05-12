d1d7bfe1c60c318ebf6cb6e9b558602e1004dafa
Riku Vartiainen <vartiainenriku@gmail.com>
2022-05-12 20:15:35 +0300

    removed redundancies

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
 import './App.css';
 import CustomerList from './components/CustomerList';
 import TrainingList from './components/TrainingList';
-import DataCharts from './components/Charts';
+
 
 function App() {
   return (
@@ -13,13 +13,11 @@ function App() {
       <Toolbar className='toolbar'>
         <NavLink to='/' style={linkStyle} className='link'>Customers </NavLink>
         <NavLink to='/trainings' style={linkStyle}  className='link'>Trainings</NavLink>
-        <NavLink to='/charts' style={linkStyle} className='link'>Charts</NavLink>
       </Toolbar>
     </AppBar>
       <Routes>
         <Route exact path='/' element={<CustomerList />} />
         <Route exact path='/trainings' element={<TrainingList />} />
-        <Route exact path='/charts' element={<DataCharts />} />
       </Routes>
     </BrowserRouter>
   </div>
